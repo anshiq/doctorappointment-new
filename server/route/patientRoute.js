26f955c518379122a285f3bcef4932fbb526eddf
appurav <appuravjaat370@gmail.com>
2024-08-28 19:36:37 +0530

    added create appointmentn route

--- server/route/patientRoute.js
+++ server/route/patientRoute.js
@@ -1,7 +1,8 @@
 const {loginUser,signupUser,forgotPassword,verifyEmailToken,verifyForgotPasswordToken} = require('../controller/patient/PatientAuthenticationHandler')
-const {getPatientAppointments,acceptYourDoctorForTreatment,reviewDoctor} = require('../controller/patient/PatientAppointmentHandler')
+const {getPatientAppointments,acceptYourDoctorForTreatment,reviewDoctor, createAppointment} = require('../controller/patient/PatientAppointmentHandler')
 const PatientRoute = require('express').Router() // jwt require 
 const PatientAuthRoute = require('express').Router() // not jwt
+PatientRoute.post('/create-appointment',createAppointment )
 PatientRoute.route('/get-patient-appointments').get(getPatientAppointments)
 PatientRoute.route('/accept-your-doctor',acceptYourDoctorForTreatment)
 PatientRoute.route('/review-doctor',reviewDoctor)
